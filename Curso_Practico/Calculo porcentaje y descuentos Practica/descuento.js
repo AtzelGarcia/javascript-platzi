95b2cf3fecfaab130942ce621dd370eb46d2a8e7
AtzelGarcia <atzel.garcia@outlook.com>
2021-08-04 12:29:59 -0500

    Reto Taller 2 calculando porcentajes y descuentos

--- Curso_Practico/Calculo porcentaje y descuentos Practica/descuento.js
+++ Curso_Practico/Calculo porcentaje y descuentos Practica/descuento.js
@@ -1,10 +1,11 @@
+//Primera solucion
 function calculoDescuento(precio, descuento) {
   const porcentajeDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajeDescuento) / 100;
   return precioConDescuento;
 }
 
-function precioFinal() {
+/* function precioFinal() {
   const inputPrecio = document.getElementById("inputPrecio");
   const valorPrecio = inputPrecio.value;
   const inputDescuento = document.getElementById("inputDescuento");
@@ -14,4 +15,50 @@ function precioFinal() {
   precioConDescuento = calculoDescuento(valorPrecio, valorDescuento);
   textoPrecio.innerText =
     "El precio final con el descuento aplicado es: " + precioConDescuento;
+} */
+
+//Segunda solucion
+const coupons = [
+  {
+    name: "cupon1",
+    discount: 15,
+  },
+  {
+    name: "cupon2",
+    discount: 30,
+  },
+  {
+    name: "cupon3",
+    discount: 25,
+  },
+];
+
+function validarCupon(valorCupon) {
+  const isCouponValueValid = function (coupon) {
+    return coupon.name === valorCupon;
+  };
+  const userCoupon = coupons.find(isCouponValueValid);
+  return userCoupon;
+}
+
+function precioFinal() {
+  const inputPrecio = document.getElementById("inputPrecio");
+  const valorPrecio = inputPrecio.value;
+  /*   const inputDescuento = document.getElementById("inputDescuento");
+  const valorDescuento = inputDescuento.value; */
+  const inputCupon = document.getElementById("inputCupon");
+  const valorCupon = inputCupon.value;
+  const textoPrecio = document.getElementById("resultadoDescuento");
+  let precioConDescuento;
+  let valorValidado;
+
+  valorValidado = validarCupon(valorCupon);
+
+  if (!valorValidado) {
+    alert("El cupon --> " + valorCupon + "<-- no es valido");
+  } else {
+    precioConDescuento = calculoDescuento(valorPrecio, valorValidado.discount);
+    textoPrecio.innerText =
+      "El precio final con el descuento aplicado es: " + precioConDescuento;
+  }
 }
